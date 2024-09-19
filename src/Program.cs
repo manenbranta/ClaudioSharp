5c87eb6126b0cbcc7368b392b027c841df278ba2
Chloe <chloe.martins@etec.sp.gov.br>
2024-09-19 16:57:13 -0300

    Start working on forca()

--- src/Program.cs
+++ src/Program.cs
@@ -441,4 +441,27 @@ class Program
     {
         return num % 2 == 0;
     }
+
+    static void forca()
+    {
+        string[] listaPalavras = [
+            "amarelo",
+            "miserável",
+            "caatinga",
+            "transeunte",
+            "quarentena",
+        ];
+
+        Dictionary<string,string> dicas = new Dictionary<string,string>();
+        dicas.Add("amarelo", "Cor do maior astro próximo da terra");
+        dicas.Add("miserável", "Antônimo da palavra pobre");
+        dicas.Add("caatinga", "Bioma brasileiro");
+        dicas.Add("transeunte", "Aquele ou aquilo em constante movimento");
+        dicas.Add("quarentena", "Sinônimo de isolação");
+
+        string fmt(string str) 
+        {
+            return str[0].ToString().ToUpper() + str.Substring(1).ToLower();
+        }
+    }
 }
