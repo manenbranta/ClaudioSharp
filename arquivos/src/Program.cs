d4e611eb1e70e790b7da13b9d987dd90ee848af2
Chloe <chloe.martins@etec.sp.gov.br>
2024-11-25 17:41:21 -0300

    Basic program structure and data

--- arquivos/src/Program.cs
+++ arquivos/src/Program.cs
@@ -1,23 +1,117 @@
 using System;
 using System.Net.Mail;
+using System.Globalization;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace arquivos;
 
 class Program
 {
+    static List<DadosUsuario> registros = new List<DadosUsuario>();
     static void Main()
     {
+        string[] menuOptions = {
+            "Novo registro", 
+            "Listar registros", 
+            "Sair"
+        };
 
+        string optStr;
+        int opt;
+        bool repetir;
+
+        do 
+        {
+            Console.WriteLine("-----CADASTRO-----");
+            Console.WriteLine("O que deseja fazer?");
+            menu(menuOptions);
+            optStr = Console.ReadLine();
+            opt = int.Parse(optStr ?? "1");
+
+            repetir = false;
+            switch (opt)
+            {
+                case 1:
+                    do
+                    {
+                        registros.Add(registro());
+                        Console.Write("Deseja adicionar mais um registro? [S/n] ");
+                        string temp = Console.ReadLine().Trim(' ');
+                        repetir = 
+                            string.Equals(temp, "S", StringComparison.CurrentCultureIgnoreCase) || 
+                            string.Equals(temp, "SIM", StringComparison.CurrentCultureIgnoreCase);
+                    } while(repetir);
+                    Console.Clear();
+                    break;
+                case 2:
+                    foreach (var usr in registros)
+                    {
+                        Console.WriteLine(usr.ToString());
+                    }
+                    Console.WriteLine("Pressione ENTER para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+                    break;
+                case 3:
+                    Console.ResetColor();
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERRO: A Opção que escolheu é inválida!");
+                    Console.WriteLine("Pressione ENTER para terminar o programa.");
+                    Console.ReadLine();
+                    Console.ResetColor();
+                    Console.Clear();
+                    Environment.Exit(1);
+                    break;
+            }
+        } while(true);
+    }
+
+    public static DadosUsuario registro()
+    {
+        Console.Clear();
+        Console.Write("Digite o nome: ");
+        string nome = Console.ReadLine();
+        Console.Write("Digite a data de nascimento: ");
+        DateTime data = DateTime.Parse(Console.ReadLine(), new CultureInfo("pt-BR"));
+        Console.Write("Digite o telefone: ");
+        string tel = Console.ReadLine();
+        string email = "";
+        bool emailError;
+        do
+        {
+            emailError = false;
+            Console.Write("Digite o email: ");
+            string addr = Console.ReadLine();
+            try {
+                email = new MailAddress(addr).Address;
+            } catch(FormatException) {
+                emailError = true;
+            }
+        } while(emailError);
+
+        return new DadosUsuario(nome, data, tel, email);
+    }
+
+    public static void menu(string[] options)
+    {
+        for (int i=0; i<options.Length; i++)
+        {
+            Console.WriteLine($"[{i+1}] - {options[i]}");
+        }
     }
 }
 
 struct DadosUsuario
 {
-    string nome;
-    DateTime nascimento;
-    string telefone;
-    string email;
+    string nome { get; set; }
+    DateTime nascimento { get; set; }
+    string telefone { get; set; }
+    string email { get; set; }
 
     public DadosUsuario(string nome, DateTime nascimento, string telefone, string email)
     {
@@ -25,15 +119,14 @@ struct DadosUsuario
         this.nascimento = nascimento;
         this.telefone = telefone;
         this.email = email;
+
+        Format();
     }
 
-    public DadosUsuario Format()
+    public void Format()
     {
-        DadosUsuario usuario = new DadosUsuario(nome, nascimento, telefone, email);
-        usuario.nome = fmtNome(usuario.nome);
-        usuario.telefone = fmtTel(usuario.telefone);
-
-        return usuario;
+        nome = fmtNome(nome);
+        telefone = fmtTel(telefone);
     }
 
     string fmtNome(string nome)
@@ -69,16 +162,23 @@ struct DadosUsuario
             }
         }
 
+        resultado = resultado.TrimStart().TrimEnd();
+
         return resultado;
     }
 
     string fmtTel(string tel) 
     {
-        string resultado = Regex.Replace(tel, @"/\D/g", "");
+        string resultado = Regex.Replace(tel, @"\D", "");
 
         if (resultado.Length > 2) resultado = $"({resultado.Substring(0,2)}) {resultado.Substring(2)}";
         if (resultado.Length > 9) resultado = $"{resultado.Substring(0,9)}-{resultado.Substring(9)}";
 
         return resultado;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {nome}, Nascimento: {nascimento:dd/MM/yyyy}, Telefone: {telefone}, Email: {email}";
     }
 }
